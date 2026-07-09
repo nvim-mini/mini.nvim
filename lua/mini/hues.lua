e356341b3dd64fe6ffeffb58688f533c7b792e9d
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2026-07-09 10:20:26 +0300

    fix(hues): drop outdated tree-sitter highlight groups
    Details:
    - "New" tree-sitter group names were introduced in Neovim=0.10. As
      support for Neovim<0.10 is dropped, defining outdated (pre rename)
      groups is not needed.

--- lua/mini/hues.lua
+++ lua/mini/hues.lua
@@ -693,184 +693,92 @@ MiniHues.apply_palette = function(palette, plugins, opts)
   hi('markdownH5', { link='@markup.heading.5' })
   hi('markdownH6', { link='@markup.heading.6' })
 
-  -- Tree-sitter
-  -- Sources:
-  -- - `:h treesitter-highlight-groups`
-  -- - https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md#highlights
-  hi('@text.literal',   { link='Special' })
-  hi('@text.reference', { link='Identifier' })
-  hi('@text.title',     { link='Title' })
-  hi('@text.uri',       { link='Underlined' })
-  hi('@text.todo',      { link='Todo' })
-  hi('@text.note',      { link='MoreMsg' })
-  hi('@text.warning',   { link='WarningMsg' })
-  hi('@text.danger',    { link='ErrorMsg' })
-  hi('@text.strong',    { fg=nil, bg=nil, bold=true          })
-  hi('@text.emphasis',  { fg=nil, bg=nil, italic=true        })
-  hi('@text.strike',    { fg=nil, bg=nil, strikethrough=true })
-  hi('@text.underline', { link='Underlined' })
+  -- Tree-sitter. Source: `:h treesitter-highlight-groups`.
+  hi('@variable',                   { fg=p.fg,   bg=nil })
+  hi('@variable.builtin',           { link='Special' })
+  hi('@variable.parameter',         { fg=p.blue, bg=nil })
+  hi('@variable.parameter.builtin', { link='@variable.builtin' })
+  hi('@variable.member',            { link='Identifier' })
 
-  hi('@comment',     { link='Comment' })
-  hi('@punctuation', { link='Delimiter' })
+  hi('@constant',         { link='Constant' })
+  hi('@constant.builtin', { link='Special' })
+  hi('@constant.macro',   { link='Macro' })
 
-  hi('@constant',          { link='Constant' })
-  hi('@constant.builtin',  { link='Special' })
-  hi('@constant.macro',    { link='Macro' })
-  hi('@define',            { link='Define' })
-  hi('@macro',             { link='Macro' })
-  hi('@string',            { link='String' })
-  hi('@string.escape',     { link='SpecialChar' })
-  hi('@string.special',    { link='SpecialChar' })
+  hi('@module',         { link='Identifier' })
+  hi('@module.builtin', { link='@variable.builtin' })
+  hi('@label',          { link='Label' })
+
+  hi('@string',                { link='String' })
+  hi('@string.documentation',  { link='@string' })
+  hi('@string.regexp',         { link='SpecialChar' })
+  hi('@string.escape',         { link='SpecialChar' })
+  hi('@string.special',        { link='SpecialChar' })
+  hi('@string.special.symbol', { link='@constant' })
+  hi('@string.special.path',   { link='Directory' })
+  hi('@string.special.url',    { link='@markup.link.url' })
+
   hi('@character',         { link='Character' })
   hi('@character.special', { link='SpecialChar' })
-  hi('@number',            { link='Number' })
-  hi('@boolean',           { link='Boolean' })
-  hi('@float',             { link='Float' })
+
+  hi('@boolean',      { link='Boolean' })
+  hi('@number',       { link='Number' })
+  hi('@number.float', { link='Float' })
+
+  hi('@type',            { link='Type' })
+  hi('@type.builtin',    { link='Special' })
+  hi('@type.definition', { link='Typedef' })
+
+  hi('@attribute',         { link='Macro' })
+  hi('@attribute.builtin', { link='Special' })
+  hi('@property',          { link='Identifier' })
 
   hi('@function',         { link='Function' })
   hi('@function.builtin', { link='Special' })
   hi('@function.call',    { link='Function' })
   hi('@function.macro',   { link='Macro' })
-  hi('@parameter',        { fg=p.blue, bg=nil })
-  hi('@method',           { link='Function' })
-  hi('@method.call',      { link='Function' })
-  hi('@field',            { link='Identifier' })
-  hi('@property',         { link='Identifier' })
-  hi('@constructor',      { link='Special' })
 
-  hi('@conditional',    { link='Conditional' })
-  hi('@repeat',         { link='Repeat' })
-  hi('@label',          { link='Label' })
-  hi('@operator',       { link='Operator' })
-  hi('@keyword',        { link='Keyword' })
-  hi('@keyword.return', { fg=p.orange, bg=nil, bold=true })
-  hi('@exception',      { link='Exception' })
+  hi('@function.method',      { link='Function' })
+  hi('@function.method.call', { link='Function' })
 
-  hi('@variable',         { fg=p.fg, bg=nil })
-  hi('@variable.builtin', { link='Special' })
-  hi('@type',             { link='Type' })
-  hi('@type.builtin',     { link='Special' })
-  hi('@type.definition',  { link='Typedef' })
-  hi('@storageclass',     { link='StorageClass' })
-  hi('@structure',        { link='Structure' })
-  hi('@namespace',        { link='Identifier' })
-  hi('@include',          { link='Include' })
-  hi('@preproc',          { link='PreProc' })
-  hi('@debug',            { link='Debug' })
-  hi('@tag',              { link='Tag' })
+  hi('@constructor', { link='Special' })
+  hi('@operator',    { link='Operator' })
 
-  hi('@symbol', { link='Keyword' })
-  hi('@none',   {})
-
-  -- Semantic tokens. Source: `:h lsp-semantic-highlight`.
-  hi('@lsp.type.class',         { link='@structure' })
-  hi('@lsp.type.decorator',     { link='@function' })
-  hi('@lsp.type.enum',          { link='@type' })
-  hi('@lsp.type.enumMember',    { link='@constant' })
-  hi('@lsp.type.function',      { link='@function' })
-  hi('@lsp.type.interface',     { link='@type' })
-  hi('@lsp.type.macro',         { link='@macro' })
-  hi('@lsp.type.method',        { link='@method' })
-  hi('@lsp.type.namespace',     { link='@namespace' })
-  hi('@lsp.type.parameter',     { link='@parameter' })
-  hi('@lsp.type.property',      { link='@property' })
-  hi('@lsp.type.struct',        { link='@structure' })
-  hi('@lsp.type.type',          { link='@type' })
-  hi('@lsp.type.typeParameter', { link='@type.definition' })
-  hi('@lsp.type.variable',      { link='@variable' })
-
-  hi('@lsp.mod.deprecated',     { fg=p.red, bg=nil })
-
-  -- New tree-sitter groups
-  -- Sources:
-  -- - `:h treesitter-highlight-groups`
-  -- - https://github.com/nvim-treesitter/nvim-treesitter/commit/1ae9b0e4558fe7868f8cda2db65239cfb14836d0
-  -- TODO: Combine with "old" tree-sitter groups into a single section
-
-  -- @variable
-  -- @variable.builtin
-  hi('@variable.parameter', { link='@parameter' })
-  hi('@variable.member',    { link='@field' })
-
-  -- @constant
-  -- @constant.builtin
-  -- @constant.macro
-
-  hi('@module',         { link='@namespace' })
-  hi('@module.builtin', { link='@variable.builtin' })
-  -- @label
-
-  -- @string
-  hi('@string.documentation',  { link='@string' })
-  hi('@string.regexp',         { link='SpecialChar' })
-  -- @string.escape
-  -- @string.special
-  hi('@string.special.symbol', { link='@constant' })
-  hi('@string.special.path',   { link='Directory' })
-  hi('@string.special.url',    { link='@markup.link.url' })
-  hi('@string.special.vimdoc', { link='@constant' })
-
-  -- @character
-  -- @character.special
-
-  -- @boolean
-  -- @number
-  hi('@number.float', { link='@float' })
-
-  -- @type
-  -- @type.builtin
-  -- @type.definition
-  hi('@type.qualifier', { link='StorageClass' })
-
-  hi('@attribute', { link='Macro' })
-  -- @property
-
-  -- @function
-  -- @function.builtin
-  -- @function.call
-  -- @function.macro
-
-  hi('@function.method',      { link='@method' })
-  hi('@function.method.call', { link='@method.call' })
-
-  -- @constructor
-  -- @operator
-
-  -- @keyword
+  hi('@keyword',           { link='Keyword' })
   hi('@keyword.coroutine', { link='@keyword' })
   hi('@keyword.function',  { link='@keyword' })
   hi('@keyword.operator',  { link='@keyword' })
-  hi('@keyword.import',    { fg=p.blue, bg=nil, bold=true })
-  hi('@keyword.storage',   { fg=p.fg,   bg=nil, bold=true })
+  hi('@keyword.import',    { fg=p.blue,   bg=nil, bold=true })
+  hi('@keyword.type',      { link='@keyword' })
+  hi('@keyword.modifier',  { link='@keyword' })
   hi('@keyword.repeat',    { link='@keyword' })
-  -- @keyword.return
-  hi('@keyword.debug',     { fg=p.cyan, bg=nil, bold=true })
+  hi('@keyword.return',    { fg=p.orange, bg=nil, bold=true })
+  hi('@keyword.debug',     { fg=p.cyan,   bg=nil, bold=true })
   hi('@keyword.exception', { link='@keyword' })
 
   hi('@keyword.conditional',         { link='@keyword' })
-  hi('@keyword.conditional.ternary', { link='keyword' })
+  hi('@keyword.conditional.ternary', { link='@keyword' })
 
   hi('@keyword.directive',        { fg=p.blue, bg=nil, bold=true })
   hi('@keyword.directive.define', { link='@keyword.directive' })
 
-  hi('@punctuation.delimiter', { link='@punctuation' })
-  hi('@punctuation.bracket',   { link='@punctuation' })
+  hi('@punctuation.delimiter', { link='Delimiter' })
+  hi('@punctuation.bracket',   { link='@punctuation.delimiter' })
   hi('@punctuation.special',   { link='Special' })
 
-  -- @comment
+  hi('@comment',               { link='Comment' })
   hi('@comment.documentation', { link='@comment' })
 
-  hi('@comment.error',   { link='@text.danger' })
-  hi('@comment.warning', { link='@text.warning' })
-  hi('@comment.todo',    { link='@text.todo' })
-  hi('@comment.note',    { link='@text.note' })
+  hi('@comment.error',   { link='ErrorMsg' })
+  hi('@comment.warning', { link='WarningMsg' })
+  hi('@comment.todo',    { link='Todo' })
+  hi('@comment.note',    { link='MoreMsg' })
 
-  hi('@markup.strong',        { link='@text.strong' })
-  hi('@markup.italic',        { link='@text.emphasis' })
-  hi('@markup.strikethrough', { link='@text.strike' })
-  hi('@markup.underline',     { link='@text.underline' })
+  hi('@markup.strong',        { fg=nil, bg=nil, bold=true })
+  hi('@markup.italic',        { fg=nil, bg=nil, italic=true })
+  hi('@markup.strikethrough', { fg=nil, bg=nil, strikethrough=true })
+  hi('@markup.underline',     { fg=nil, bg=nil, underline=true })
 
-  hi('@markup.heading',   { link='@text.title' })
+  hi('@markup.heading',   { link='Title' })
   hi('@markup.heading.1', { fg=p.orange, bg=nil })
   hi('@markup.heading.2', { fg=p.yellow, bg=nil })
   hi('@markup.heading.3', { fg=p.green,  bg=nil })
@@ -878,32 +786,56 @@ MiniHues.apply_palette = function(palette, plugins, opts)
   hi('@markup.heading.5', { fg=p.azure,  bg=nil })
   hi('@markup.heading.6', { fg=p.blue,   bg=nil })
 
-  hi('@markup.heading.1.delimiter.vimdoc', { fg=p.bg_mid2, bg=nil, bold=true })
-  hi('@markup.heading.2.delimiter.vimdoc', { fg=p.bg_mid2, bg=nil, bold=true })
-  hi('@markup.heading.4.vimdoc', { link='Title' })
-
   hi('@markup.quote',       { link='@string.special' })
   hi('@markup.math',        { link='@string.special' })
-  hi('@markup.environment', { link='@module' })
 
-  hi('@markup.link',       { link='@text.reference' })
+  hi('@markup.link',       { link='Identifier' })
   hi('@markup.link.label', { link='@markup.link' })
   hi('@markup.link.url',   { fg=p.fg, bg=nil, underline=true })
 
-  hi('@markup.raw',       { link='@text.literal' })
+  hi('@markup.raw',       { link='Special' })
   hi('@markup.raw.block', { link='@markup.raw' })
 
   hi('@markup.list',           { link='@punctuation.special' })
   hi('@markup.list.checked',   { link='DiagnosticOk' })
   hi('@markup.list.unchecked', { link='DiagnosticWarn' })
 
-  hi('@diff.plus',  { link='diffAdded' })
-  hi('@diff.minus', { link='diffRemoved' })
-  hi('@diff.delta', { link='diffChanged' })
+  hi('@diff.plus',  { link='Added' })
+  hi('@diff.minus', { link='Removed' })
+  hi('@diff.delta', { link='Changed' })
 
-  -- @tag
+  hi('@tag',           { link='Tag' })
+  hi('@tag.builtin',   { link='Special' })
   hi('@tag.attribute', { link='@tag' })
-  hi('@tag.delimiter', { link='@punctuation' })
+  hi('@tag.delimiter', { link='Delimiter' })
+
+  hi('@none', {})
+
+  -- Tree-sitter for selected languages
+  -- - Vimdoc (built-in help)
+  hi('@markup.heading.1.delimiter.vimdoc', { fg=p.bg_mid2, bg=nil, bold=true })
+  hi('@markup.heading.2.delimiter.vimdoc', { fg=p.bg_mid2, bg=nil, bold=true })
+  hi('@markup.heading.4.vimdoc',           { link='Title' })
+  hi('@string.special.vimdoc',             { link='@constant' })
+
+  -- Semantic tokens. Source: `:h lsp-semantic-highlight`.
+  hi('@lsp.type.class',         { link='Structure' })
+  hi('@lsp.type.decorator',     { link='@function' })
+  hi('@lsp.type.enum',          { link='@type' })
+  hi('@lsp.type.enumMember',    { link='@constant' })
+  hi('@lsp.type.function',      { link='@function' })
+  hi('@lsp.type.interface',     { link='@type' })
+  hi('@lsp.type.macro',         { link='Macro' })
+  hi('@lsp.type.method',        { link='@function.method' })
+  hi('@lsp.type.namespace',     { link='@module' })
+  hi('@lsp.type.parameter',     { link='@variable.parameter' })
+  hi('@lsp.type.property',      { link='@property' })
+  hi('@lsp.type.struct',        { link='Structure' })
+  hi('@lsp.type.type',          { link='@type' })
+  hi('@lsp.type.typeParameter', { link='@type.definition' })
+  hi('@lsp.type.variable',      { link='@variable' })
+
+  hi('@lsp.mod.deprecated',     { fg=p.red, bg=nil })
 
   -- Plugins
   -- nvim-mini/mini.nvim
