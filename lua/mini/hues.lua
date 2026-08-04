8cef23520832134c05e4524e49f0468623501053
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2026-08-04 14:05:57 +0300

    fix(hues): make `@lsp.mod.defaultLibrary` same as other "builtin" groups

--- lua/mini/hues.lua
+++ lua/mini/hues.lua
@@ -835,6 +835,7 @@ MiniHues.apply_palette = function(palette, plugins, opts)
   hi('@lsp.type.typeParameter', { link='@type.definition' })
   hi('@lsp.type.variable',      { link='@variable' })
 
+  hi('@lsp.mod.defaultLibrary', { link='Special' })
   hi('@lsp.mod.deprecated',     { fg=p.red, bg=nil })
 
   -- Plugins
